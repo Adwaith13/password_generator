88fa7faf9d1e213c5a7807b157449912fc1697e9
Adwaith Nair <adwaith1310@gmail.com>
2023-07-01 18:28:08 +0530

    added function generating password

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-var slider = document.getElementById("range");
-var output = document.getElementById("char-len");
-output.innerHTML = slider.value;
-slider.oninput = function() {
-    output.innerHTML = this.value;
+//value for slider
+function charlength(){
+  let range=document.getElementById("range");
+  let characterlength=document.getElementById("char-len");
+  characterlength.innerHTML=range.value
 }
-output.style.color='#A4FFAF';
-output.style.fontSize='20px';
+charlength();
+
+//function for generating password
+function generatepassword(){
+  const uppercaseChecked=document.getElementById("uppercase").checked;
+  const lowercaseChecked=document.getElementById("lowercase").checked;
+  const numbersChecked=document.getElementById("numbers").checked;
+  const symbolsChecked=document.getElementById("symbols").checked;
+  const slider=document.getElementById("range").value;
+
+  let password='';
+  const uppercaseChar='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowercaseChar='abcdefghijklmnopqrstuvwxyz';
+  const numberChar='0123456789';
+  const symbolChar='!@#$%^&*()_-+=<>?';
+
+  if(uppercaseChecked){
+    password+=uppercaseChar;
+  }
+  if(lowercaseChecked){
+    password+=lowercaseChar;
+  }
+  if(numbersChecked){
+    password+=numberChar;
+  }
+  if(symbolsChecked){
+    password+=symbolChar;
+  }
+
+  let generated_pass='';
+  for(let i=0;i<slider;i++)
+  {
+    generated_pass+=password.charAt(Math.floor(Math.random()*password.length));
+  }
+  document.getElementById('password').innerHTML=generated_pass;
+}
+
+
+
+
+
+  
